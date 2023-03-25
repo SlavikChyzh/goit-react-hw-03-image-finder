523cb0a3848d1dbf34d021e644ce55544f08c3ad
SlavikChyzh <110846906+SlavikChyzh@users.noreply.github.com>
2023-03-25 19:34:01 +0200

    done
    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    searchWorld: '',
+  };
+
+  searchbarSubmit = world => {
+    this.setState({ searchWorld: world });
+  };
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.searchbarSubmit} />
+        <ImageGallery search={this.state.searchWorld}></ImageGallery>
+      </>
+    );
+  }
+}
